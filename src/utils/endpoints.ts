ffb32f0f3e873720c47147b99b58b77721d8e659
Mohammed <iraqiProfessionalDesigner@gmail.com>
2023-07-19 14:52:23 +0700

    updates in enpoints

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -2,7 +2,7 @@
 // the API is from Node.js on the same server. 
 
 // const baseURL = "https://mohammed-api.vercel.app";
-const baseURL = "https://free-restapi.vercel.app/"
+const baseURL = "https://free-restapi.vercel.app"
 
 export const Endpoints = {
     getUsers: ({ q }: { q?: string } ): string => `${baseURL}/api/users${q ? `?q=${q}` : ''}`,
